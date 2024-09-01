973c26c982be147c9258bbe83b94dd46a7e94670
AnthonyArias04 <anthonyarias842@gmail.com>
2024-09-01 07:40:31 -0600

    Cambio

--- Javascript/sintaxis.js
+++ Javascript/sintaxis.js
@@ -1,1 +1,44 @@
 console.log("HOLIWIS IMPRIMIMOS EN LA CONSOLA");
+
+// ----------OPERADORES MATEMATICOS----------
+
+let suma = 2 + 2;
+let resta = 5 - 3;
+let division = 500 / 5;
+let multiplicacion = 1354 * 1356;
+let residuo = 528 % 10;
+
+console.log(suma);
+console.log(resta);
+console.log(division);
+console.log(multiplicacion);
+console.log(residuo);
+
+// CONCATENACION -> UNIR DOS VALORES QUE TIENEN QUE SER EL DATO DE TIPO (STRING)
+//Al unir dos valores y cualquiera de los dos es un string transformara todo a string EJEMPLO:
+
+let sumaFalsa = 2 + "2";
+console.log(sumaFalsa);
+
+// --------- OPERADORES LOGICOS -----------
+// EJECUTAMOS COMPARACIONES PUEDEN SER DE IGUALDAD O DESIGUALDAD
+
+let igualdad = 10 == "10";
+console.log(igualdad);
+
+// IGUALDAD ESTRICTA === COMPARA EL VALOR DE LAS COSAS Y SUS TIPOS DE DATOS
+
+let igualdadEstricta = 10 === "10";
+console.log(igualdadEstricta);
+
+// DESIGUALDAD != COMPARAMOS QUE LOS VALORES SON DIFERENTES
+
+let desigualdad = 10 != "10";
+console.log(desigualdad);
+
+let desigualdadEstricta = 10 !== "10";
+console.log(desigualdadEstricta);
+
+let operadorAND = 10 == "10" && 10 === "10";
+console.log(operadorAND);
+

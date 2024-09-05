7ea775541c05c058725510e8cab06a65ebf54deb
AnthonyArias04 <anthonyarias842@gmail.com>
2024-09-05 17:40:01 -0600

    Hola

---
+++ DOM/ejercicio.js
@@ -0,0 +1,19 @@
+/*
+EJERCICIO:
+REALIZA UNA FUNCION QUE RECIBA UN ARRAY NUMERICO Y RETORNE COMPLETAMENTE NUEVO 
+SOLAMENTE CON LOS NUMEROS QUE SEAN MULTIPLOS DE 3 Y QUE LA SUMA DE TODOS SUS NUMEROS DE MENOS DE 100
+
+*/
+
+function multiplos3 (multi){
+    for(i=0; i<100; i++){
+        console.log(i);
+        
+    }
+
+}
+multiplos3(multi)
+
+console.log("Holiwis");
+
+

ddc9ebc1d50613c60745901577f06879de86b029
AnthonyArias04 <anthonyarias842@gmail.com>
2024-09-09 20:11:03 -0600

    CLASE DE HOY 9 DE SEPTIEMBRE

---
+++ index.ts
@@ -0,0 +1,24 @@
+//Programacion Orientada a Objetos
+//Es una forma de programar intentando siempre describir o pensar las cosas como si fueran objetos
+
+//Conceptos principales de POO -> Clases y los objetos 
+//Clases -> Molde para generar algo.
+//Objeto -> Lo que podemos crear a base de ese molde.
+
+//DECLARACION DE CLASES
+
+class   Auto{
+    //Pensar primero en las caracteristicas generales de todos los autos -> SE LE LLAMA: Propiedades o Atributos
+
+    numChasis: number;
+    motor:string;
+    color:string;
+    tipoCombustible:string;
+    transmision:string;
+    cantidadKilometro:string;
+    cantPasajros:number;
+    frenos:string;
+    modelo:string;
+    añoFab: number;
+
+}

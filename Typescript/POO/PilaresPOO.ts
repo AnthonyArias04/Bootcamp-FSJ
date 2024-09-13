b2a6edc7873350af7c199d1e1e0ba75987f020cc
AnthonyArias04 <anthonyarias842@gmail.com>
2024-09-12 21:29:58 -0600

    CLASE 12 DE SEPTIEMBRE

--- Typescript/POO/PilaresPOO.ts
+++ Typescript/POO/PilaresPOO.ts
@@ -1,8 +1,8 @@
 //4 Pilares POO 
 
 /*
-Herencia
-Polimorfismo
+Herencia -> Es la capacidad de una subclase de obtener todo de su clase PADRE
+Polimorfismo ->  
 
 Encasuplamiento -> Podemos limitar el acceso a algo a traves de MODIFICADORES DE ACCESO
 Abstraccion -> Nos da herramientas para interactuar con lo que este limitado de una clase.
@@ -38,8 +38,12 @@ class person{
         return this.name
     }
 
-    setAge(ageParam):number{
+    setAge(ageParam:number){
         this.age = ageParam
+    }
+
+    getAge(ageParam:number){
+        this.age = ageParam;
     }
 }
 
@@ -48,3 +52,29 @@ let personita = new person("Anthony Arias", 20, "06598850-2");
 //personita.age = 15;
 console.log(personita);
 
+class Developer extends person{
+    private exp:number;
+    private techSkills:string;
+    private softSkills:string[];
+    private gitUser:string; 
+    private area:string;
+    private projects:string[];
+
+    constructor(nameParam:string, ageParam:number, duiParam:string, expParam: number, techSkillsParam:string, softSkillsParam:string[], gitUserParam: string, areaParam: string, projectParam:string[]){
+        super(nameParam, ageParam, duiParam);
+
+        this.exp = expParam;
+        this.techSkills = techSkillsParam;
+        this.softSkills = softSkillsParam;
+        this.gitUser = gitUserParam;
+        this.area = areaParam;
+        this.projects = projectParam;
+    }
+    getArea(){
+        return this.area;
+    }
+
+}
+
+let developercito = new Developer("Anthony Arias", 20, "0205048-3", 1, "C#, JAVA, JAVASCRIPT, TYPESCRIPT, HTML, CSS, BOOSTRAP",["Comunicacion efectiva, Proactivo, Trabajo en equipo, Liderazgo, Autocritica"], "AnthonygitUser", "Backend Developer", ["APIS"]);
+developercito.getArea();

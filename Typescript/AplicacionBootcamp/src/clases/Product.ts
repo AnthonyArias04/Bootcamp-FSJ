17601970d0b99caab4926095bc618243c56a7f55
AnthonyArias04 <anthonyarias842@gmail.com>
2024-09-17 21:55:43 -0600

    CLASE 17 DE SEPTIEMBRE

--- Typescript/AplicacionBootcamp/src/clases/Product.ts
+++ Typescript/AplicacionBootcamp/src/clases/Product.ts
@@ -1,4 +1,14 @@
-class Producto{
+export class Producto{
     private id: number;
     private nombre: string
+    private precio: number
+    private cantidad: number
+
+    constructor(idParam:number, nombreParam: string, precioParam: number, cantidadParam: number){
+        this.id=idParam
+        this.nombre=nombreParam
+        this.precio=precioParam
+        this.cantidad=cantidadParam
+    }
 }
+
--- Typescript/AplicacionBootcamp/src/main.ts
+++ Typescript/AplicacionBootcamp/src/main.ts
@@ -1,38 +1,49 @@
+import { Producto } from './clases/Product';
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Holiwis desde el MainTs</h1>
-    <p id = "parrafo"></p>
 
-    <form id="loginForm">
-    <section>
-    <label>Nombre de usuario</label>
-    <input type= "text" id= "username" name="name" placeholder="Ingresa tu nombre de usuario">
-
-    </section>
-
-    <section>
-    <label>Password</label>
-    <input type="password" id="password" placeholder="Ingresa tu password">
-    </section>
-
-    <button type="submit">Iniciar Sesion</button>
-    </form>
+document.querySelector<HTMLDivElement>('#app')!.innerHTML=
+ ` <div>
+      <h1>Gestion de tiendita</h1>
+      <form id="formProducto">
+        <section>
+          <label>Nombre Producto</label>
+          <input type="text" id="nombre" name="nombre" placeholder="Ingresa el nombre del producto">
+        </section>
+        <section>
+        <label>Precio</label>
+        <input type="text" id="precio" name="precio" placeholder="Ingresa su precio">
+        </section>
+        <section>
+        <label>Cantidad</label>
+        <input type="text" id="cantidad" name="cantidad" placeholder="Ingresa su cantidad">
+        </section>
+        <button type="submit">Agregar producto</button>
+      </form>
   </div>
 `
 
 //document.querySelector<HTMLElement>("#parrafo")!.innerText = "Este es texto desde el p";
 
-const form = document.getElementById("loginForm") as HTMLFormElement;
+
+
+const form = document.getElementById("formProducto") as HTMLFormElement;
 
 form.addEventListener("submit",(e:SubmitEvent) => {
    e.preventDefault(); 
   console.log("Holiwis");
 
-  const username = (document.getElementById("username") as HTMLInputElement).value;
-  const password = (document.getElementById("password") as HTMLInputElement).value;
-  console.log(username);
-  console.log(password);
+  const id = Date.now();
+
+  const nombre = (document.getElementById("nombre") as HTMLInputElement).value;
+  const precio = parseFloat((document.getElementById("precio") as HTMLInputElement).value);
+  const cantidad = parseInt((document.getElementById("cantidad") as HTMLInputElement).value);
+  console.log(nombre);
+  console.log(precio);
+  console.log(cantidad);
+  
+  let productito = new Producto(id, nombre, precio, cantidad)
+  console.log(productito);
+  localStorage.setItem("productos", JSON.stringify(productito))
   
 })

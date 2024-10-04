9a136cb8845ac064005326dd9dec792fa0220cba
AnthonyArias04 <anthonyarias842@gmail.com>
2024-10-03 21:21:23 -0600

    CUBO HOY

--- React/auth-app/src/pages/session/Session.jsx
+++ React/auth-app/src/pages/session/Session.jsx
@@ -1,7 +1,27 @@
-
+import { useForm } from 'react-hook-form';
 
 export const Session = () => {
+
+  const {register, handleSubmit} = useForm();
+
+  const onSubmitForm = (e) =>{
+    e.preventDefault();
+    console.log("Se envio el form");
+    
+  }
   return (
-    <div>HOLIWIS</div>
+    <div>
+      <form onSubmit={onSubmitForm}>
+        <label> Email </label>
+        <input type="text" id="email" placeholder="Example: email@mail.com" {... register ('email')}/>
+        <label> Password </label>
+        <input type="password" id="password" placeholder="Enter your password"{... register ('password')}/>
+        <label> Repeat your Password</label>
+        <input type="password" id="password" placeholder="Repeat your password" {... register ('confirm password')}/>
+        <button type="submit">Send</button>
+      </form>
+    </div>
   )
 }
+
+//Destructuring -> Desestructurar un objeto 

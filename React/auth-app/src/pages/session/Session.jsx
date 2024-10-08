4cc036cf26d350e5894b43948e42f147d0f77620
AnthonyArias04 <anthonyarias842@gmail.com>
2024-10-08 17:43:14 -0600

    Cambios

--- React/auth-app/src/pages/session/Session.jsx
+++ React/auth-app/src/pages/session/Session.jsx
@@ -1,23 +1,47 @@
-import { useForm } from 'react-hook-form';
+import {useForm} from 'react-hook-form'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase/config';
 
 export const Session = () => {
 
   const {register, handleSubmit} = useForm();
 
-  const onSubmitForm = (e) =>{
-    e.preventDefault();
+  const onSubmitForm = (data) =>{
+    /*e.preventDefault();
     console.log("Se envio el form");
+    /*console.log(e.target.email.value);
+    console.log(e.target.password.value);
+    console.log(e.target.confirmPassword.value);*/
+    console.log(data);
+
+  createUserWithEmailAndPassword(auth, data.email, data.password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+    
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(error);
+    
+    // ..
+  });
+
     
   }
+
   return (
     <div>
-      <form onSubmit={onSubmitForm}>
+      <form onSubmit={handleSubmit(onSubmitForm)}>
         <label> Email </label>
         <input type="text" id="email" placeholder="Example: email@mail.com" {... register ('email')}/>
         <label> Password </label>
         <input type="password" id="password" placeholder="Enter your password"{... register ('password')}/>
         <label> Repeat your Password</label>
-        <input type="password" id="password" placeholder="Repeat your password" {... register ('confirm password')}/>
+        <input type="password" id="password" placeholder="Repeat your password" {... register ('confirmPassword')}/>
         <button type="submit">Send</button>
       </form>
     </div>

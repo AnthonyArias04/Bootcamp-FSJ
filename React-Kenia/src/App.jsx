4e6f9e823c0f36df250d323b757390d015cb2930
AnthonyArias04 <anthonyarias842@gmail.com>
2024-10-09 16:07:12 -0600

    AVANCE REACT

---
+++ React-Kenia/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Header from './assets/Header'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header fullname="Anthony Arias"/>
+    </>
+  )
+}
+
+export default App

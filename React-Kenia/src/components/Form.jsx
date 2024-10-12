06239ab4a02e93d1fbbe408c13ab9591cea62cb3
AnthonyArias04 <anthonyarias842@gmail.com>
2024-10-12 15:39:12 -0600

    AVANCE CLASES

--- React-Kenia/src/components/Form.jsx
+++ React-Kenia/src/components/Form.jsx
@@ -1,13 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Form() {
-  return (
-    <form action=''>
-        <input type='text' placeholder='Ingresa titulo'/>
 
-        <textarea placeholder='descripcion'></textarea>
+  //Para manejar los estados utilizaremos los HOOKS utilizando UseState -> maneja el estado de tu componente
+
+  //DECLARANDO UN ESTADO
+  /**
+   * Manejo dos valores: 
+   * [0] -> Se guarda como se inicializa el estado.
+   * [1] -> Se encarga de actualizar el estado.
+   */
+  const [titleNote, setTitleNote] = useState ("")
+  const [descriptionNote, setDescriptionNote] = useState ("")
+  
+  //Estado para guardar Notas
+  const [listNotes, setListNotes] = useState([])
+
+  //Funcion para capturar el titulo de la nota:
+
+  const handleTitle = (e) =>{
+    
+    //Cuando encontres la etiqueta captura su valor
+    //console.log(e.target.value);
+    //Actualizamos el estado
+    setTitleNote(e.target.value)
+  }
+  const handleDescription = (e) =>{
+    
+    //Cuando encontres la etiqueta captura su valor
+    //console.log(e.target.value);
+    //Actualizamos el estado
+    setDescriptionNote(e.target.value)
+  }
+  
+  //metodo para guardar Notas
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    //Necesitamos guardar la nota
+
+    //spread operate (...) hacemos una copia del arreglo y guardamos la nueva nota
+    setListNotes([...listNotes,{
+      title: titleNote,
+      description: descriptionNote
+    }])
+  }
+
+  console.log(listNotes);
+  
+
+  return (
+    <div>
+    <form action='' onSubmit={handleSubmit}>
+        <input type='text' placeholder='Ingresa titulo' onChange={(e) => handleTitle(e)}/>
+
+        <textarea placeholder='descripcion' onChange={(e) => handleDescription(e)}></textarea>
 
         <input type='submit' value="Guardar nota"/>
     </form>
+    <h2>Iterando las notas</h2>
+    {
+      listNotes.map((note) =>{
+        return(
+          <p>Titulo de la nota: {note.title}</p>
+        )
+      })
+    }
+    </div>
   )
 }
